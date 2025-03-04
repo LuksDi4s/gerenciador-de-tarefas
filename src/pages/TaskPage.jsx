6e87cc5dfdbffe140b825ace568ac47545bbbf24
LuksDi4s <luuks177@gmail.com>
2025-03-03 22:31:25 -0300

    fix

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -7,7 +7,7 @@ function TaskPage() {
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6 ">
+    <div className="h-screen w-screen flex justify-center bg-slate-500 p-6">
       <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative mb-6">
           <button
